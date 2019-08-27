5c925d16a141342c3a8b0c075c0261744c1f13d2
lindmark421@gmail.com <lindmark421@gmail.com>
2019-08-27 10:51:47 -0400

    Add project files.

---
+++ WebApiRESTDemoProject/Models/WebServiceDb.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiRESTDemoProject.Models {
+    public class WebServiceDb : DbContext {
+
+
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Vendor> Vendors { get; set; }
+
+        public WebServiceDb(DbContextOptions options) : base(options) 
+            {
+
+        }
+
+        string c = @"Data Source=DESKTOP-8R894VL\SQLEXPRESS;Initial Catalog = WebAPIDemo; Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+    }
+}
